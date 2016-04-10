1df3dcb5ee29d77328866e5228638eeea9fb11ea
Diksha <Diksha@Dikshas-MacBook-Air.local>
2016-04-10 01:36:44 -0400

    email notifications implemented

--- working code/muse_pyliblo_server.py
+++ working code/muse_pyliblo_server.py
@@ -1,30 +1,71 @@
-from liblo import *
-import matplotlib.pyplot as plt
-
+import signal
 import sys
-import time
+import time as t
 import numpy as np
+import smtplib
+import matplotlib.pyplot as plt
+import smtplib
+from email.MIMEMultipart import MIMEMultipart
+from email.MIMEText import MIMEText
+from email.MIMEBase import MIMEBase
+from email import encoders
+from liblo import *
 
+
+INTERVAL = 100 
+MAX_SD_SUM = -1
+
+def send_email_notif():
+    fromaddr = ""
+    toaddr = ""
+ 
+    msg = MIMEMultipart()
+ 
+    msg['From'] = fromaddr
+    msg['To'] = toaddr
+    msg['Subject'] = "Cognitive health session report"
+     
+    body = "Please find attached the report"
+ 
+    msg.attach(MIMEText(body, 'plain'))
+ 
+    filename = "report.png"
+    attachment = open("EEG.png", "rb")
+ 
+    part = MIMEBase('application', 'octet-stream')
+    part.set_payload((attachment).read())
+    encoders.encode_base64(part)
+    part.add_header('Content-Disposition', "attachment; filename= %s" % filename)
+ 
+    msg.attach(part)
+ 
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.starttls()
+    server.login(fromaddr, "")
+    text = msg.as_string()
+    server.sendmail(fromaddr, toaddr, text)
+    server.quit()
+
+def signal_handler(signal, frame):
+    send_email_notif()
+    sys.exit(0)
+
+signal.signal(signal.SIGINT, signal_handler)
+
+EEG = [0]*4
+EEG_SD = [0]*4
 
 plt.ion()
 plt.ylim([50,1000])
-INTERVAL = 100 
-sum_sd = 0.0
 
-eeg_le = [0] * INTERVAL
-eeg_lf = [0] * INTERVAL
-eeg_re = [0] * INTERVAL
-eeg_rf = [0] * INTERVAL
+for i in range(4):
+    EEG[i]= [0] * INTERVAL
+    EEG_SD[i]= [0] * INTERVAL
 
-eeg_le_sd = [0] * INTERVAL
-eeg_lf_sd = [0] * INTERVAL
-eeg_re_sd = [0] * INTERVAL
-eeg_rf_sd = [0] * INTERVAL
-
-line_le, = plt.plot(eeg_le_sd)
-line_lf, = plt.plot(eeg_lf_sd)
-line_re, = plt.plot(eeg_re_sd)
-line_rf, = plt.plot(eeg_rf_sd)
+line_le, = plt.plot(EEG_SD[0])
+line_lf, = plt.plot(EEG_SD[1])
+line_re, = plt.plot(EEG_SD[2])
+line_rf, = plt.plot(EEG_SD[3])
 
 plt.draw()
 plt.legend(['Left Ear', 'Left Forehead', 'Right Ear', 'Right Forehead'], loc='upper left')
@@ -47,38 +88,35 @@ class MuseServer(ServerThread):
     #receive EEG data
     @make_method('/muse/eeg', 'ffff')
     def eeg_callback(self, path, args):
+        global MAX_SD_SUM
         l_ear, l_forehead, r_forehead, r_ear = args
-        eeg_le.append(l_ear)
-        eeg_lf.append(l_forehead)
-        eeg_re.append(r_ear)
-        eeg_rf.append(r_forehead)
+        EEG[0].append(l_ear)
+        EEG[1].append(l_forehead)
+        EEG[2].append(r_ear)
+        EEG[3].append(r_forehead)
         
         # print "LE = ",np.std(eeg_le)
         # print "LF = ",np.std(eeg_lf)
         # print "RE = ",np.std(eeg_re)
         # print "RF = ",np.std(eeg_rf)
-        del eeg_le[0]
-        del eeg_lf[0]
-        del eeg_re[0]
-        del eeg_rf[0]
-
-        del eeg_le_sd[0]
-        del eeg_lf_sd[0]
-        del eeg_re_sd[0]
-        del eeg_rf_sd[0]
-
-        eeg_le_sd.append(np.std(eeg_le))
-        eeg_lf_sd.append(np.std(eeg_lf))
-        eeg_re_sd.append(np.std(eeg_re))
-        eeg_rf_sd.append(np.std(eeg_rf))
+        sd_sum = 0.0
+        for i in range(4):
+            del EEG[i][0]
+            del EEG_SD[i][0]
+            EEG_SD[i].append(np.std(EEG[i]))
+            sd_sum += EEG_SD[i][-1]
         
-        line_le.set_ydata(eeg_le_sd)
-        line_lf.set_ydata(eeg_lf_sd)
-        line_re.set_ydata(eeg_re_sd)
-        line_rf.set_ydata(eeg_rf_sd)
+        
+        line_le.set_ydata(EEG_SD[0])
+        line_lf.set_ydata(EEG_SD[1])
+        line_re.set_ydata(EEG_SD[2])
+        line_rf.set_ydata(EEG_SD[3])
         plt.draw()
-
         plt.pause(0.1)
+        if MAX_SD_SUM<sd_sum:
+            MAX_SD_SUM = sd_sum
+            plt.savefig("EEG.png")
+        
     
     #print "hello123"
         #print "%s %f %f %f %f" % (path, l_ear, l_forehead, r_forehead, r_ear)
@@ -110,4 +148,4 @@ server.start()
 
 if __name__ == "__main__":
     while 1:
-        time.sleep(1)
+        t.sleep(1)
